94065a3384733819361ee89c10aff88adebcef30
babayigit <babayigit@gmail.com>
2024-05-30 10:04:10 +0300

    20240529

---
+++ src/day19_ConstructorCollStaticKeyword/P01_ConstructorCall.java
@@ -0,0 +1,49 @@
+package day19_ConstructorCollStaticKeyword;
+
+public class P01_ConstructorCall {
+
+    P01_ConstructorCall(){
+        // Call to 'this()' must be first statement in constructor body
+         this("Levent",40);
+        //this("Murat");
+        // this(7);
+        // Only one explicit constructor call allowed in constructor
+        System.out.println("Parametresiz constructor çalıştı");
+        // P01_ConstructorCall("Murat"); //Method call expected
+        // İsim(parametre) yazimi sadece methodlar için kabul edilir
+        // Constructor call için
+        //this keywordu yardımıyla call yapılır
+
+    }
+
+    P01_ConstructorCall(String isim){
+
+        System.out.println("İsim parametreli constructor çalıştı");
+    }
+
+    P01_ConstructorCall(int sayi){
+        System.out.println("Sayı parametreli constructor çalıştı");
+    }
+
+    P01_ConstructorCall(String isim, int sayi){
+        System.out.println("İsim ve Sayı parametreli constructor çalıştı");
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Main Method çalıştı");
+        method1();
+
+        P01_ConstructorCall obj=new P01_ConstructorCall();
+
+    }
+
+    public static void method1(){
+        method2();
+        System.out.println("Method 1 Çalıştı");
+    }
+
+    public static void method2(){
+        System.out.println("Method 2 Çalıştı");
+    }
+}
---
+++ src/day19_ConstructorCollStaticKeyword/P02_Constructor.java
@@ -0,0 +1,63 @@
+package day19_ConstructorCollStaticKeyword;
+
+public class P02_Constructor {
+    String isim="Ahmet";
+
+    public static void main(String[] args) {
+            P02_Constructor obj1=new P02_Constructor();
+
+        System.out.println((obj1.isim)); //Ahmet
+        obj1.isim="Hamza";
+        System.out.println((obj1.isim)); //Hamza
+
+        obj1.method2(); // Static olmayan method çalıştı
+        obj1.method1(); // Static method çalıştı
+                        //Static member '' accessed via instance reference
+        method1(); // Static method çalıştı
+
+        System.out.println( "========= Eşitliğin sağ tarafını alarak kullanalım ==========");
+
+        System.out.println(new P02_Constructor().isim); // Ahmet
+        new P02_Constructor().method2(); // Static olmayan method çalıştı
+        /*
+            eğer sadece new Construcor_name(); şeklinde bir kullanımda
+            sadece o satır için bir obje oluşur ve işini yapar.
+            o satırdan alt satıra geçince obje yok olur
+         */
+
+        System.out.println( "========= Eşitliğin sol tarafını alarak kullanaral null değer atayalım ==========");
+
+        P02_Constructor obj2=null;
+
+        // System.out.println(obj2.isim); //NullPointerException
+        // obj2.method2(); //NullPointerException
+
+        obj2.method1();  // Static method çalıştı
+        /*
+        Constructor kullanmaz ve obj null olarak işaretlenirse
+        normal bir objenin yaptıkalrını yapamaz
+        nullpointerexception verir
+
+       static olmayan methodlarda çağrılamaz
+       fatak static methodlar bu null değer atanan obje ile çağrılabilir ve sorunsuz çalışır
+
+
+
+
+         */
+
+
+
+
+
+
+    }
+
+    public static void method1(){
+        System.out.println("Static method çalıştı");
+    }
+
+    public void method2(){
+        System.out.println("Static olmayan method çalıştı");
+    }
+}
